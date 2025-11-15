641b05fe1a2d7d49384ce822aa5a2e37acddc3a6
Dmytro Chyrva <dmytro.chyrva.dev@gmail.com>
2025-11-15 18:23:41 +0100

    [00008] MVP Commit.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomePage } from './pages/home-page/home-page';
+import { ProjectsPage } from './pages/projects-page/projects-page';
+import { AboutPage } from './pages/about-page/about-page';
+import { ServicesPage } from './pages/services-page/services-page';
+import { ContactPage } from './pages/contact-page/contact-page';
+import { NotFoundPage } from './pages/not-found-page/not-found-page';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePage },
+  { path: 'projects', component: ProjectsPage },
+  { path: 'about', component: AboutPage },
+  { path: 'services', component: ServicesPage },
+  { path: 'contact', component: ContactPage },
+  { path: '**', component: NotFoundPage },
+];
